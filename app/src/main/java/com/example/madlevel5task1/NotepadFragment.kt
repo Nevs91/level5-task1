4c40315cf156ac637fd2c1ee97c23c156b391adc
Sven van Putten <svenvanputten@gmail.com>
2020-11-09 14:09:01 +0100

    Connected the NoteViewModel to the NotepadFragment and created a method to observe the viewModel

--- app/src/main/java/com/example/madlevel5task1/NotepadFragment.kt
+++ app/src/main/java/com/example/madlevel5task1/NotepadFragment.kt
@@ -1,17 +1,20 @@
 package com.example.madlevel5task1
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.madlevel5task1.views.NoteViewModel
+import kotlinx.android.synthetic.main.fragment_notepad.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class NotepadFragment : Fragment() {
+
+    private val viewModel: NoteViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +26,17 @@ class NotepadFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        observeAddNoteResult()
+    }
+
+    private fun observeAddNoteResult() {
+        viewModel.note.observe(viewLifecycleOwner, { note ->
+            note?.let {
+                tvNoteTitle.text = it.title
+                tvLastUpdated.text = getString(R.string.last_updated, it.lastUpdated.toString())
+                tvNoteText.text = it.text
+            }
+        })
     }
 }
